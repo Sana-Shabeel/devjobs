a2e3ba19b747d8ff6c77b099ca0490efdbb1f447
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-12 15:57:14 +0300

    Add extend theme with custom width and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      width: {
+        327: "20.4375rem",
+        339: "21.1875rem",
+        351: "21.9375rem",
+        689: "43.0625rem",
+        1110: "69.375rem",
+      },
+      colors: {
+        violet: "#5964E0",
+        lightViolet: "#939bf4",
+        darkBlue: "#19202D",
+        midnight: "#121721",
+        lightGray: "#F4F6F8",
+        gray: "#9DAEC2",
+        darkGray: "#6E8098",
       },
     },
   },
   plugins: [],
-}
+};
