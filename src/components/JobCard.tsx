7512f985bbc7c62782d7f42ce082e725863209b3
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-12 18:01:51 +0300

    Add data dynamically

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,29 +1,35 @@
 import React from "react";
 import Image from "next/image";
+interface JobCardProps {
+  job: {
+    company: string;
+    logo: string;
+    logoBackground: string;
+    position: string;
+    postedAt: string;
+    contract: string;
+    location: string;
+  };
+}
 
-const JobCard = () => {
+const JobCard = ({ job }: JobCardProps) => {
   return (
     <div className="w-327  md:w-339 xl:w-351 mx-auto px-5 pb-6 rounded-md  bg-white my-6">
       <div
         className="w-14 h-14 flex justify-center items-center rounded-xl relative top-[-22px] "
-        style={{ background: "hsl(36, 87%, 49%)" }}
+        style={{ background: job.logoBackground }}
       >
-        <Image
-          src="/assets/logos/scoot.svg"
-          width="44"
-          height="44"
-          alt="company logo"
-        />
+        <Image src={job.logo} width="44" height="44" alt="company logo" />
       </div>
       <div>
         <div className="flex justify-start items-center text-gray gap-5 mb-3 ">
-          <span>5h ago</span>
+          <span>{job.postedAt}</span>
           <span className="w-1 h-1 rounded bg-gray"></span>
-          <span className="text-gray ">Full Time</span>
+          <span className="text-gray ">{job.contract}</span>
         </div>
-        <h1 className="text-xl font-bold">Senior Software Engineer</h1>
-        <span className="block text-gray mb-8">Scoot</span>
-        <h3 className="text-violet font-bold ">United Kingdom</h3>
+        <h1 className="text-xl font-bold">{job.position}</h1>
+        <span className="block text-gray mb-8">{job.company}</span>
+        <h3 className="text-violet font-bold ">{job.location}</h3>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,17 @@
 import Image from "next/image";
 import { Kumbh_Sans } from "next/font/google";
 import JobCard from "@/components/JobCard";
+import data from "../../data/data.json";
 
 const kumbh = Kumbh_Sans({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={`${kumbh.className} py-24`}>
-      <section className="container md:w-689 lg:w-1110 border">
-        <JobCard />
-        <JobCard />
-        <JobCard />
-        <JobCard />
-        <JobCard />
-        <JobCard />
-        <JobCard />
-        <JobCard />
-        <JobCard />
-        <JobCard />
-        <JobCard />
-        <JobCard />
+      <section className="container md:w-689 lg:w-1110 ">
+        {data.map((job) => (
+          <JobCard key={job.id} job={job} />
+        ))}
       </section>
     </main>
   );
