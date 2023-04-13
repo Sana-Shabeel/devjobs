65b29c93327fcdd47cbeb2440c19a09bed5a9bce
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-13 15:56:32 +0300

    Add jobcard comp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import { Kumbh_Sans } from "next/font/google";
 import JobCard from "@/components/JobCard";
 import data from "../../data/data.json";
+import Button from "@/components/Button";
+import FilterTab from "@/components/FilterTab/FilterTab";
 
 const kumbh = Kumbh_Sans({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={`${kumbh.className} py-24`}>
+    <main className={`${kumbh.className} py-6`}>
+      <FilterTab />
       <section className="container md:w-689 lg:w-1110 ">
         {data.map((job) => (
           <JobCard key={job.id} job={job} />
