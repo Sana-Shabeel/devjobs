1091b537513867d05f368b67bceeec6b5ff7fd45
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-14 15:50:58 +0300

    Add chakra modal

--- src/components/FilterTab/FilterTab.tsx
+++ src/components/FilterTab/FilterTab.tsx
@@ -2,14 +2,37 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Filter from "./Filter";
 import RadioInput from "./RadioInput";
+import { useDisclosure } from "@chakra-ui/react";
+import FilterModal from "../Modal/FilterModal";
 
 const FilterTab = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const [isMobile, setIsMobile] = useState(false);
 
-  const [selectedValue, setSelectedValue] = useState("");
+  const [selectedValue, setSelectedValue] = useState(false);
 
-  const handleInputChange = (value: string) => {
+  const [inputValue, setInputValue] = useState({
+    title: "",
+    location: "",
+  });
+
+  const handleRadioChange = (value: boolean) => {
     setSelectedValue(value);
+  };
+
+  const handleInputChange = (value: {}) => {
+    setInputValue({ ...inputValue, ...value });
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log({
+      fulltime: selectedValue,
+      title: inputValue.title,
+      location: inputValue.location,
+    });
   };
 
   useEffect(() => {
@@ -30,53 +53,72 @@ const FilterTab = () => {
   }, []);
 
   return (
-    <form className="mx-auto my-6 flex h-[5rem] w-327 items-center rounded-md bg-white md:w-auto lg:justify-start">
-      <Filter
-        icon="/assets/desktop/icon-search-violet.svg"
-        placeholder={
-          isMobile
-            ? "Filter by title"
-            : "Filter by title, companies, expertise."
-        }
-        width="lg:basis-[45%] ml-8"
+    <>
+      <FilterModal
+        isOpen={isOpen}
+        onClose={onClose}
+        handleRadioChange={handleRadioChange}
+        selectedValue={selectedValue}
+        handleInputChange={handleInputChange}
       />
-      <div className="DIVIDER mx-3 hidden h-[5rem] w-[1px] bg-lightGray md:block " />
-      <Filter
-        icon="/assets/desktop/icon-location.svg"
-        placeholder="Filter by location..."
-        width="lg:basis-[20%]"
-        hidden="hidden"
-      />
-      <div className="DIVIDER mx-3 hidden h-[5rem] w-[1px] bg-lightGray md:block " />
-
-      <div className="mx-4 cursor-pointer md:hidden">
-        <Image
-          src="/assets/mobile/icon-filter.svg"
-          className=""
-          width={20}
-          height={20}
-          alt="search icon"
+      <form
+        onSubmit={onSubmit}
+        className="mx-auto my-6 flex h-[5rem] w-327 items-center rounded-md bg-white md:w-auto lg:justify-start"
+      >
+        <Filter
+          icon="/assets/desktop/icon-search-violet.svg"
+          placeholder={
+            isMobile
+              ? "Filter by title"
+              : "Filter by title, companies, expertise."
+          }
+          width="lg:basis-[45%] ml-8"
+          name="title"
+          onChange={handleInputChange}
         />
-      </div>
-
-      <RadioInput
-        label="Full Time"
-        value="option1"
-        checked={selectedValue === "option1"}
-        onChange={handleInputChange}
-      />
-
-      <button className="mr-6 grid h-12 w-12 place-items-center rounded bg-violet md:w-20 lg:w-32">
-        <Image
-          src="/assets/desktop/icon-search-white.svg"
-          className="md:hidden "
-          width={20}
-          height={20}
-          alt="search icon"
+        <div className="DIVIDER mx-3 hidden h-[5rem] w-[1px] bg-lightGray md:block " />
+        <Filter
+          icon="/assets/desktop/icon-location.svg"
+          placeholder="Filter by location..."
+          width="lg:basis-[20%]"
+          hidden="hidden"
+          name="location"
+          onChange={handleInputChange}
         />
-        <span className="hidden text-center text-white md:inline">Search</span>
-      </button>
-    </form>
+        <div className="DIVIDER mx-3 hidden h-[5rem] w-[1px] bg-lightGray md:block " />
+        <div onClick={onOpen} className="mx-4 cursor-pointer md:hidden">
+          <Image
+            src="/assets/mobile/icon-filter.svg"
+            className=""
+            width={20}
+            height={20}
+            alt="search icon"
+          />
+        </div>
+        <RadioInput
+          label="Full Time"
+          value="Fulltime"
+          checked={selectedValue}
+          onChange={handleRadioChange}
+        />
+        <button
+          type="submit"
+          onClick={onSubmit}
+          className="mr-2 grid h-12 w-12 place-items-center rounded bg-violet md:mr-4 md:w-20 lg:w-32"
+        >
+          <Image
+            src="/assets/desktop/icon-search-white.svg"
+            className="md:hidden "
+            width={20}
+            height={20}
+            alt="search icon"
+          />
+          <span className="hidden text-center text-white md:inline">
+            Search
+          </span>
+        </button>
+      </form>
+    </>
   );
 };
 
