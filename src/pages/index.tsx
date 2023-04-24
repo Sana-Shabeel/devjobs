cc38e5f99debc773cfd07a384d1715461db75373
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-24 16:39:54 +0300

    Add headless ui modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,15 @@ import JobCard from "@/components/JobCard";
 import data from "../../data/data.json";
 import Button from "@/components/Button";
 import FilterTab from "@/components/FilterTab/FilterTab";
-import FilterModal from "@/components/Modal/FilterModal";
+import TestModal from "@/components/Modal/TestModal";
+import MyModal from "@/components/Modal/FilterModal";
 
 const kumbh = Kumbh_Sans({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`${kumbh.className} mx-auto overflow-hidden bg-lightGray py-6 md:w-689  lg:w-1110`}
+      className={`${kumbh.className} mx-auto overflow-hidden border bg-lightGray py-6  md:w-689 lg:w-1110`}
     >
       <FilterTab />
 
