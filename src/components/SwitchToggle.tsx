593d68801d40bf1e28f9773aa1f0ff93e06c4503
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-25 15:29:25 +0300

    Add switch toggle

---
+++ src/components/SwitchToggle.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
+
+export default function SwitchToggle() {
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <div className="">
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={`relative inline-flex
+          h-[24px] w-[48px] shrink-0 cursor-pointer items-center rounded-full  border-2 border-transparent bg-white transition-colors duration-200 ease-in-out focus:outline-none  focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+      >
+        <span className="sr-only">Use setting</span>
+        <span
+          aria-hidden="true"
+          className={`${enabled ? "translate-x-7" : "translate-x-0"}
+            pointer-events-none inline-block h-[15px] w-[15px] transform rounded-full bg-violet shadow-lg ring-0 transition duration-200 ease-in-out`}
+        />
+      </Switch>
+    </div>
+  );
+}
