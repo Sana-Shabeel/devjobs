eb46f031153a000b49aa4ab7c305d6d1b3e4ef93
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-25 15:30:04 +0300

    Add jobcard

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -14,22 +14,24 @@ interface JobCardProps {
 
 const JobCard = ({ job }: JobCardProps) => {
   return (
-    <div className="w-327  md:w-339 xl:w-351 mx-auto px-5 pb-6 rounded-md  bg-white my-6">
+    <div className="mx-auto  my-6 w-327 rounded-md bg-white px-5 pb-6  md:w-339 xl:w-351">
       <div
-        className="w-14 h-14 flex justify-center items-center rounded-xl relative top-[-22px] "
+        className="relative top-[-22px] flex h-14 w-14 items-center justify-center rounded-xl "
         style={{ background: job.logoBackground }}
       >
         <Image src={job.logo} width="44" height="44" alt="company logo" />
       </div>
       <div>
-        <div className="flex justify-start items-center text-gray gap-5 mb-3 ">
+        <div className="mb-3 flex items-center justify-start gap-5 text-gray ">
           <span>{job.postedAt}</span>
-          <span className="w-1 h-1 rounded bg-gray"></span>
+          <span className="h-1 w-1 rounded bg-gray"></span>
           <span className="text-gray ">{job.contract}</span>
         </div>
-        <h1 className="text-xl font-bold">{job.position}</h1>
-        <span className="block text-gray mb-8 mt-2">{job.company}</span>
-        <h3 className="text-violet font-bold ">{job.location}</h3>
+        <h1 className="text-xl font-bold text-darkBlue">{job.position}</h1>
+        <span className="mb-8 mt-2 block font-light tracking-wide text-gray">
+          {job.company}
+        </span>
+        <h3 className="font-bold text-violet ">{job.location}</h3>
       </div>
     </div>
   );
