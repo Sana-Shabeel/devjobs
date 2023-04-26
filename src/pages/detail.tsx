48dea8cfc9354600eaa268ee355ca6e0d2e3022c
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-26 07:49:10 +0300

    Add detail page

---
+++ src/pages/detail.tsx
@@ -0,0 +1,11 @@
+import DetailNavbar from "@/components/DetailNavbar";
+import Header from "@/components/Header";
+
+export default function detail() {
+  return (
+    <main>
+      <Header />
+      <DetailNavbar />
+    </main>
+  );
+}
