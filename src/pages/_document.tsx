aeba0b978656701f1f2d5749d3f04f85235f694e
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-26 07:06:28 +0300

    Add dark theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-lightGray text-darkBlue dark:bg-midnight dark:text-lightGray">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
