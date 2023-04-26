a0391ed019144987621ee61ac4b5aa155dc7c98e
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-26 07:05:59 +0300

    Add dark theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,25 +4,25 @@ import SwitchToggle from "./SwitchToggle";
 
 const Header = () => {
   return (
-    <header className="h-[136px] bg-hero-pattern-mobile bg-cover bg-no-repeat md:bg-hero-pattern-tablet lg:bg-hero-pattern">
-      <nav className="mx-auto flex w-327 items-center justify-between pt-8 md:w-689 lg:w-1110">
+    <header className="h-[140px] bg-hero-pattern-mobile bg-cover bg-no-repeat md:bg-hero-pattern-tablet lg:h-[10rem] lg:bg-hero-pattern 2xl:h-[15rem]">
+      <nav className="mx-auto flex w-327 items-center justify-between pt-6 md:w-689 md:pt-12 xl:w-1110 2xl:pt-24">
         <Image
           src="/assets/desktop/logo.svg"
           width="115"
           height="32"
           alt="sun icon"
         />
-        <div className="flex items-center justify-between gap-2">
+        <div className="flex items-center justify-between gap-2 md:gap-4">
           <Image
             src="/assets/desktop/icon-sun.svg"
-            width="24"
+            width="15"
             height="24"
             alt="sun icon"
           />
           <SwitchToggle />
           <Image
             src="/assets/desktop/icon-moon.svg"
-            width="24"
+            width="15"
             height="24"
             alt="moon icon"
           />
