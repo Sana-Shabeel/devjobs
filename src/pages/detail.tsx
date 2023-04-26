27497df94ff0c84df1ca9948c9b55021a8274d38
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-26 15:36:51 +0300

    Add detail page

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,11 +1,13 @@
 import DetailNavbar from "@/components/DetailNavbar";
 import Header from "@/components/Header";
+import JobDetail from "@/components/JobDetail";
 
 export default function detail() {
   return (
     <main>
       <Header />
       <DetailNavbar />
+      <JobDetail />
     </main>
   );
 }
