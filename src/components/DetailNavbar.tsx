4dc9c1af1b8eaa337d1eaae4449b8320ab988301
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-26 15:36:01 +0300

    Add detail navbar lg

--- src/components/DetailNavbar.tsx
+++ src/components/DetailNavbar.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 
 const DetailNavbar = () => {
   return (
-    <div className="mx-auto mt-[-1rem] flex w-327 flex-col items-center justify-center gap-6 rounded-md bg-white pb-6 dark:bg-darkBlue">
+    <div
+      className="mx-auto mt-[-1rem] flex w-327 flex-col items-center justify-center  
+    gap-6 rounded-md bg-white pb-6 dark:bg-darkBlue md:h-36 md:w-689 md:flex-row md:justify-between md:pb-0 md:pr-8 xl:w-[730px] "
+    >
       <div
-        className="mt-[-1.5rem] grid h-14 w-14 place-items-center items-center rounded-xl"
+        className="mt-[-1.5rem] grid h-14 w-14 place-items-center items-center rounded-xl md:m-0 md:h-36 md:w-36 md:rounded-l-md md:rounded-r-none"
         style={{ background: "hsl(36, 87%, 49%)" }}
       >
         <Image
@@ -16,8 +19,8 @@ const DetailNavbar = () => {
         />
       </div>
 
-      <div className="text-center">
-        <h2 className="">Scoot</h2>
+      <div className="right-24 text-center md:relative md:text-left xl:right-28">
+        <h2 className="text-2xl font-bold tracking-wide ">Scoot</h2>
         <span className="text-gray">scoot.com</span>
       </div>
 
