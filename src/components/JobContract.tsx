36386d925da00f39b832f072cb1466ae4a496b8e
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-28 01:28:03 +0300

    Fix data types

--- src/components/JobContract.tsx
+++ src/components/JobContract.tsx
@@ -10,7 +10,7 @@ const JobContract = ({ date, contract }: Props) => {
     <div className="mb-3 flex items-center justify-start gap-5 text-darkGray">
       <span>1w ago</span>
       <span className="h-1 w-1 rounded bg-gray"></span>
-      <span className="text-darkGray">Part Time</span>
+      <span className="text-darkGray">{contract}</span>
     </div>
   );
 };
