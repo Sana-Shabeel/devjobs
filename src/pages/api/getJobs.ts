9937b80ea299a5e5f4472b61db44a3584a669637
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-28 01:25:37 +0300

    Add fetch jobs endpoint

---
+++ src/pages/api/getJobs.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    prisma.jobs
+      .findMany()
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((error) => {
+        res.status(500).json({ error: error });
+      });
+  }
+}
