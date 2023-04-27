fe9deee39a3bac147eefd8c4b86bfc5ce3c355af
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-27 04:00:47 +0300

    Add job contract

--- src/components/JobContract.tsx
+++ src/components/JobContract.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 interface Props {
-  date: string;
-  contract: string;
+  date: string | undefined;
+  contract: string | undefined;
 }
 
 const JobContract = ({ date, contract }: Props) => {
   return (
-    <div className="mb-3 flex items-center justify-start gap-5 text-gray ">
+    <div className="mb-3 flex items-center justify-start gap-5 text-darkGray">
       <span>1w ago</span>
       <span className="h-1 w-1 rounded bg-gray"></span>
-      <span className="text-gray">Part Time</span>
+      <span className="text-darkGray">Part Time</span>
     </div>
   );
 };
