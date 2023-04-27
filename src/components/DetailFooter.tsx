cf28c7fb6fa508a8b8ff5c3d4609b9da3c08a1e5
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-27 03:56:23 +0300

    Add detail footer

---
+++ src/components/DetailFooter.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+interface Props {
+  title: string | undefined;
+}
+
+const DetailFooter = ({ title }: Props) => {
+  return (
+    <div className="bg-white dark:bg-darkBlue">
+      <div className="mx-auto flex justify-between md:w-689 xl:w-[730px]">
+        <div className=" hidden rounded-md bg-white p-6 dark:bg-darkBlue md:block ">
+          <h2 className="text-xl font-bold text-inherit md:text-2xl">
+            {title}
+          </h2>
+          <p className="font-light leading-loose text-darkGray">
+            So Digital Inc.
+          </p>
+        </div>
+        <div className="mx-auto text-center md:mx-0">
+          <button className="mb-3 mt-8 rounded-md bg-violet px-[7.5rem] py-3 font-medium tracking-wide text-white md:px-10">
+            Apply Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailFooter;
