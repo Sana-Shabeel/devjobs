5809f10231a4805964f03c53c8a714efd22aed51
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-27 03:58:18 +0300

    Add detail navbar

--- src/components/DetailNavbar.tsx
+++ src/components/DetailNavbar.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import Image from "next/image";
+import { Job } from "@/Types/job";
 
-const DetailNavbar = () => {
+interface Props {
+  job: Job | undefined;
+}
+
+const DetailNavbar = ({ job }: Props) => {
+  const website = job?.company?.split(" ").join("").toLowerCase() + ".com";
   return (
     <div
-      className="mx-auto mt-[-1rem] flex w-327 flex-col items-center justify-center  
-    gap-6 rounded-md bg-white pb-6 dark:bg-darkBlue md:h-36 md:w-689 md:flex-row md:justify-between md:pb-0 md:pr-8 xl:w-[730px] "
+      className="mx-auto mt-[-1rem] flex w-327 flex-col items-center  
+    justify-center gap-6 rounded-md bg-white pb-6 dark:bg-darkBlue md:h-36 md:w-689 md:flex-row md:justify-between md:pb-0 md:pr-8 xl:w-[730px]"
     >
       <div
         className="mt-[-1.5rem] grid h-14 w-14 place-items-center items-center rounded-xl md:m-0 md:h-36 md:w-36 md:rounded-l-md md:rounded-r-none"
-        style={{ background: "hsl(36, 87%, 49%)" }}
+        style={{ background: job?.logoBackground }}
       >
-        <Image
-          src="/assets/logos/scoot.svg"
-          width="50"
-          height="50"
-          alt="logo"
-        />
+        <Image src={job?.logo as string} width="20" height="20" alt="logo" />
       </div>
 
-      <div className="right-24 text-center md:relative md:text-left xl:right-28">
-        <h2 className="text-2xl font-bold tracking-wide ">Scoot</h2>
-        <span className="text-gray">scoot.com</span>
+      <div className=" text-center md:relative md:-left-12 md:text-left xl:-left-20">
+        <h2 className="text-2xl font-bold tracking-wide ">{job?.company}</h2>
+        <span className="mt-3 font-light text-darkGray">{website}</span>
       </div>
 
       <div className="rounded-md bg-[#EEEFFC] px-6 py-3 dark:bg-[#1F273F]">
