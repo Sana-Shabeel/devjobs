222f246983a2c2a339ab9056dec0978628c170e0
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-27 03:58:31 +0300

    Add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import SwitchToggle from "./SwitchToggle";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="h-[140px] bg-hero-pattern-mobile bg-cover bg-no-repeat md:bg-hero-pattern-tablet lg:h-[10rem] lg:bg-hero-pattern 2xl:h-[15rem]">
       <nav className="mx-auto flex w-327 items-center justify-between pt-6 md:w-689 md:pt-12 xl:w-1110 2xl:pt-24">
-        <Image
-          src="/assets/desktop/logo.svg"
-          width="115"
-          height="32"
-          alt="sun icon"
-        />
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/assets/desktop/logo.svg"
+            width="115"
+            height="32"
+            alt="sun icon"
+          />
+        </Link>
         <div className="flex items-center justify-between gap-2 md:gap-4">
           <Image
             src="/assets/desktop/icon-sun.svg"
