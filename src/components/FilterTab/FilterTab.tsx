77b365f3f13a57b658935115c6e17a00808e32ea
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-28 10:46:00 +0300

    Fix layout responsivness

--- src/components/FilterTab/FilterTab.tsx
+++ src/components/FilterTab/FilterTab.tsx
@@ -4,6 +4,7 @@ import Filter from "./Filter";
 import RadioInput from "./RadioInput";
 import FilterModal from "../Modal/FilterModal";
 import { FilterType } from "@/pages";
+import Divide from "../Divide";
 
 interface Props {
   filterData: (filter: FilterType) => void;
@@ -44,7 +45,7 @@ const FilterTab = ({ filterData }: Props) => {
   };
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    const mediaQuery = window.matchMedia("(max-width: 1201px)");
 
     const handleResize = (event: MediaQueryListEvent) => {
       setIsMobile(event.matches);
@@ -59,6 +60,9 @@ const FilterTab = ({ filterData }: Props) => {
       mediaQuery.removeEventListener("change", handleResize);
     };
   }, []);
+
+  //md:pr-0 md:w-20 lg:w-32
+  // w-[60%] lg:basis-[50%] lg:m-8
 
   return (
     <>
@@ -75,34 +79,34 @@ const FilterTab = ({ filterData }: Props) => {
 
       <form
         onSubmit={onSubmit}
-        className="mx-auto mb-8 mt-[-3rem] flex h-[5rem] w-327 items-center justify-between rounded-md bg-white pr-3 dark:bg-darkBlue md:w-689 md:pr-0 lg:justify-start xl:w-1110"
+        className="mx-auto mb-8 mt-[-3rem] flex h-[5rem] w-327 items-center justify-between rounded-md bg-white px-4 dark:bg-darkBlue sm:w-689 lg:justify-start xl:w-1110"
       >
         <Filter
           icon="/assets/desktop/icon-search-violet.svg"
           placeholder={
             isMobile
-              ? "Filter by title"
+              ? "Filter by title..."
               : "Filter by title, companies, expertise..."
           }
-          width="w-[60%] lg:basis-[50%] ml-4 lg:m-8"
+          width="sm:w-[80%] sm:ml-8 md:w-[35%] md:ml-0 xl:w-[50%]"
           name="title"
           onChange={handleInputChange}
           showIcon={false}
         />
-        <div className="DIVIDER mx-3 hidden h-[5rem] w-[1px] bg-lightGray dark:bg-darkGray md:block" />
+        <Divide className="mx-3 hidden h-[5rem] w-[1px]" />
         <Filter
           icon="/assets/desktop/icon-location.svg"
           placeholder="Filter by location…"
-          width="lg:basis-[30%] lg:ml-3"
+          width="md:basis-[30%] md:ml-3"
           hidden="hidden"
           showIcon={false}
           name="location"
           onChange={handleInputChange}
         />
-        <div className="DIVIDER mx-3 hidden h-[5rem] w-[1px] bg-lightGray dark:bg-darkGray md:block " />
+        <Divide className="mx-3 hidden h-[5rem] w-[1px]" />
         <div
           onClick={() => setShowModal(true)}
-          className="mx-3 w-28 cursor-pointer overflow-hidden md:hidden"
+          className="mx-3 w-28 cursor-pointer overflow-hidden sm:pl-10 md:hidden"
         >
           <Image
             src="/assets/mobile/icon-filter.svg"
@@ -118,22 +122,24 @@ const FilterTab = ({ filterData }: Props) => {
           onChange={handleRadioChange}
           hidden="hidden"
         />
-        <button
-          type="submit"
-          onClick={onSubmit}
-          className="mr-2 grid h-12 w-full place-items-center rounded bg-violet hover:bg-lightViolet md:mr-4 md:w-20 lg:w-32"
-        >
-          <Image
-            src="/assets/desktop/icon-search-white.svg"
-            className="md:hidden "
-            width={20}
-            height={20}
-            alt="search icon"
-          />
-          <span className="hidden text-center text-white md:inline">
-            Search
-          </span>
-        </button>
+        <div className="rounded bg-violet hover:bg-lightViolet">
+          <button
+            className="grid h-12 w-12 place-items-center sm:w-20"
+            type="submit"
+            onClick={onSubmit}
+          >
+            <Image
+              src="/assets/desktop/icon-search-white.svg"
+              className="md:hidden "
+              width={20}
+              height={20}
+              alt="search icon"
+            />
+            <span className="hidden text-center text-white md:inline">
+              Search
+            </span>
+          </button>
+        </div>
       </form>
     </>
   );
