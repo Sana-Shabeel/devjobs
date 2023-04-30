c3d1840d808e8df673f2c262cdc0d43a140f9180
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-01 02:19:22 +0300

    Add footer

--- src/components/DetailFooter.tsx
+++ src/components/DetailFooter.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 interface Props {
   title: string | undefined;
+  company: string | undefined;
 }
 
-const DetailFooter = ({ title }: Props) => {
+const DetailFooter = ({ title, company }: Props) => {
   return (
     <div className="bg-white dark:bg-darkBlue">
       <div className="mx-auto flex justify-between md:w-689 xl:w-[730px]">
@@ -11,9 +12,7 @@ const DetailFooter = ({ title }: Props) => {
           <h2 className="text-xl font-bold text-inherit md:text-2xl">
             {title}
           </h2>
-          <p className="font-light leading-loose text-darkGray">
-            So Digital Inc.
-          </p>
+          <p className="font-light leading-loose text-darkGray">{company}</p>
         </div>
         <div className="mx-auto text-center md:mx-0">
           <button className="mb-3 mt-8 rounded-md bg-violet px-[7.5rem] py-3 font-medium tracking-wide text-white hover:bg-lightViolet md:px-10">
