6a217a3a39d29837ea894626d556afd9c66fb98d
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-02 09:21:12 +0300

    Check for .env variables

--- src/pages/api/getJobs.ts
+++ src/pages/api/getJobs.ts
@@ -15,6 +15,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         res.status(200).json(data);
       })
       .catch((error) => {
+        console.log("error", error);
+
         res.status(500).json({ error: error });
       });
   }
